5db0fd83ae775a9984cefb26b5474cb218ce301f
Nagashka <e.potdevin@rubika-edu.com>
2019-01-17 08:48:21 +0100

    Gamefinishable

---
+++ Assets/Naga_Assets/Scripts/Naga_Win.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Naga_Win : MonoBehaviour {
+
+    public GameObject endMenu;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider win)
+    {
+        if (win.gameObject.tag == "Player")
+        {
+            endMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+}
