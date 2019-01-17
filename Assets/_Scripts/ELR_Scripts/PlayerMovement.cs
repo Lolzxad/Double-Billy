ea28f3825920ec95e0edb8cde3a82bc54e838cb1
Unknown <a.dourgnon@rubika-edu.com>
2019-01-17 03:03:01 +0100

    Script de déplacement

--- Assets/_Scripts/ELR_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/ELR_Scripts/PlayerMovement.cs
@@ -4,14 +4,54 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    float gravity = -10f;
     float playerSpeed = 5f;
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+    Animator anim;
+    Rigidbody rb;
+    SpriteRenderer spr;
+    public Collider checkGround;
+    private bool isGrounded = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody> ();
+        anim = GetComponent<Animator> ();
+        spr = GetComponent<SpriteRenderer> ();
+        checkGround = GetComponent<Collider> ();
+    
 
-        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
-        gameObject.GetComponent<Rigidbody>().velocity = movement * playerSpeed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+       
+        float horizontal = Input.GetAxis("Horizontal");
+
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 translation = new Vector3(horizontal, 0, vertical) * playerSpeed * Time.deltaTime;
+
+        transform.Translate(translation);
+
+       
+
+        if (checkGround == true)
+        {
+            anim.SetBool ("isGrounded", true);
+
+        }
+        else
+        {
+            anim.SetBool("isGrounded", false);
+        }
 	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Ground")
+        {
+            isGrounded = true;
+            Debug.Log("mdr le sol collideiieqbvmoz nvm sfmo verfr");
+        }
+    }
+
 }
