553d55c8421ea35cfc9b12aa484bf740ed08652d
Ludovic E <ludovic.eugenot@hotmail.fr>
2019-01-17 04:12:12 +0100

    Commit Zbra3, dangers minimes

--- Assets/_Scripts/LUE_Scripts/EnemyHealth.cs
+++ Assets/_Scripts/LUE_Scripts/EnemyHealth.cs
@@ -6,78 +6,88 @@ public class EnemyHealth : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private bool isGrounded;
+    private bool leftPlayer = false;
+    private bool rightPlayer = false;
 
-    //Initialisation enfants
+    private IEnumerator coroutine;
+
+    #region Initialisation enfants
     public GameObject groundCheck;
     public GameObject leftSide;
     public GameObject rightSide;
-    private Collider2D groundCollider;
-    private Collider2D leftCollider;
-    private Collider2D rightCollider;
+    public GameObject rangeGrab;
+    public GameObject rangePunch;
+    public GameObject rangeKick;
+    private Collider groundCollider;
+    private Collider leftCollider;
+    private Collider rightCollider;
+    private Collider rangeGrabCollider;
+    private Collider rangePunchCollider;
+    private Collider rangeKickCollider;
+    #endregion
 
     //Stats de l'ennemi 
     [SerializeField]
-    float enemyMaxHealth = 100f;
+    float enemyMaxHealth = 5f;
     float enemyHealth = 75f;
+
+    //Stats de dégâts
     [SerializeField]
-    float enemyRegenPerSec = 30f;
+    float punchDamage = 1f;
     [SerializeField]
-    float enemyIdleHealthLoss = 60f;
+    float kickDamage = 2f;
     [SerializeField]
-    float enemyHit = 100f;
+    float grabDamage = 3f;
+
+    float punchKnockbackTime = 0.2f;
+    float kickKnockbackTime = 0.4f;
+    float grabKnockbackTime = 0.6f;
+
 
     private void Start()
     {
         enemyHealth = enemyMaxHealth;
         rb2d = GetComponent<Rigidbody2D>();
-        groundCollider = groundCheck.GetComponent<Collider2D>();
-        leftCollider = leftSide.GetComponent<Collider2D>();
-        rightCollider = rightSide.GetComponent<Collider2D>();
+        groundCollider = groundCheck.GetComponent<Collider>();
+        leftCollider = leftSide.GetComponent<Collider>();
+        rightCollider = rightSide.GetComponent<Collider>();
+        rangeGrabCollider = rangeGrab.GetComponent<Collider>();
+        rangePunchCollider = rangePunch.GetComponent<Collider>();
+        rangeKickCollider = rangeKick.GetComponent<Collider>();
     }
 
-    private void FixedUpdate()
+    /*private void OnTriggerEnter(Collider other)
     {
-        //if (groundCollider)
+        if(other==leftCollider)
+    }*/
 
-        //fait monter la vie :      à remplacer par "si les enchaînements se passent bien"
-        if (Input.GetKey(KeyCode.Space))
+    private void OnTriggerStay(Collider other, string punchKickGrab, Collider side)
+    {
+        if ((punchKickGrab == "punch" || punchKickGrab == "punch2") && other.tag == "Player")
         {
-            enemyHealth += enemyRegenPerSec * Time.deltaTime;
+            EnemyHealthLoss(punchDamage);
+            if (leftPlayer)
+            {
+                
+            }
+            else if (rightPlayer)
+            {
+
+            }
         }
-
-        //fait descendre la vie :       à remplacer par "si l'ennemi ne fait rien"
-        if (!Input.anyKey)
+        else if ((punchKickGrab == "kick" || punchKickGrab == "kick2") && other.tag == "Player")
         {
-            enemyHealth -= enemyIdleHealthLoss * Time.deltaTime;
+            EnemyHealthLoss(kickDamage);
         }
-
-        //l'ennemi se prend un coup (sur appui de  : à remplacer par "si l'ennemi ne fait rien")
-        if (Input.GetMouseButtonDown(0))
+        else if ((punchKickGrab == "grab" || punchKickGrab == "grab2") && other.tag == "Player")
         {
-            enemyHealth -= enemyHit;
-        }
-
-        //empêche la vie de passer le maximum de vie
-        if (enemyHealth >= enemyMaxHealth)
-        {
-            enemyHealth = enemyMaxHealth;
-        }
-
-        //si la vie arrive à 0, l'ennemi meurt (dans la console pour l'instant)
-        if (enemyHealth <= 0)
-        {
-            print("T'es mort !");
-
-            //empêche de passer trop loin sous 0 pour les tests
-            enemyHealth = 0;
+            EnemyHealthLoss(grabDamage);
         }
     }
 
     public void EnemyHealthLoss(float hitReceived)
     {
         enemyHealth -= hitReceived;
-
-        //rb2d.velocity=
 
         //si la vie arrive à 0, l'ennemi meurt (dans la console pour l'instant)          ramener animation de mort
         if (enemyHealth <= 0)
@@ -89,6 +99,20 @@ public class EnemyHealth : MonoBehaviour
         }
     }
 
-    
+    /*private IEnumerator Knockback(float waitTime)
+    {
+        if (leftCollider.tag == "Player")
+        {
 
+        }
+        else if (rightCollider.tag == "Player")
+        {
+
+        }
+    }*/
+
+    /*private IEnumerator Death()
+    {
+
+    }*/
 }
